3650b67b7bcbbd77b8b9c703567fd459a379085a
Emil <konikiewiczemil@gmail.com>
2022-05-06 13:05:53 +0200

    remove fetch promise wrapper, fix types

--- src/features/userInfo/userInfoAPI.ts
+++ src/features/userInfo/userInfoAPI.ts
@@ -3,5 +3,4 @@ import { UserInfo } from "./userInfoSlice"
 export function fetchUserInfo(login: string): Promise<UserInfo> {
   return fetch(`https://api.github.com/users/${login}`)
     .then(response => response.json())
-    .then(data => data)
 }
--- src/features/usersList/usersListAPI.ts
+++ src/features/usersList/usersListAPI.ts
@@ -1,7 +1,6 @@
-export function fetchUsers(amount = 30): Promise<any> {
-  return new Promise((resolve) =>
-    fetch(`https://api.github.com/users?per_page=${amount}`)
-      .then(response => response.json())
-      .then(data => resolve(data))
-  );
+import { UserData } from "./usersListSlice";
+
+export function fetchUsers(amount = 30): Promise<UserData[]> {
+  return fetch(`https://api.github.com/users?per_page=${amount}`)
+    .then(response => response.json())
 }
